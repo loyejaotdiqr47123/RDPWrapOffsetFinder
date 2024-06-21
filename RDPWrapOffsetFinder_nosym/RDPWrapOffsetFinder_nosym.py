c36e20e063e169ddbfe5d12418b0bfe0c9062fd8
bobo <bobo134662@proton.me>
2024-06-21 09:07:57 +0000

    翻译

--- RDPWrapOffsetFinder_nosym/RDPWrapOffsetFinder_nosym.py
+++ RDPWrapOffsetFinder_nosym/RDPWrapOffsetFinder_nosym.py
@@ -13,3 +13,24 @@ def find_section_by_name(pe_file_path, section_name):
             return section  # 返回指向该节头的指针（即section对象）
     
     return None
+
+def find_pattern_in_pe(pe_file, pattern):
+    pe = pefile.PE(pe_file, fast_load=True)
+        
+    for section in pe.sections:
+        # 获取内存区段的虚拟地址和大小
+        virtual_addr = section.VirtualAddress
+        size_of_data = section.SizeOfRawData
+            
+        # 读取内存区段的内容
+        pe_file.seek(section.PointerToRawData)
+        data = pe_file.read(size_of_data)
+            
+        # 在内存区段中搜索字节模式
+        offset = data.find(pattern)
+        if offset != -1:
+            return virtual_addr + offset
+        
+        # 如果没有找到匹配的字节模式，返回-1
+    
+    return -1
